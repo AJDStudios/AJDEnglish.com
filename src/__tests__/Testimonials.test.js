dbd5dd69f9a8c214604407fd911602ce2d1f5e98
AD (AJDStudios) <105710407+AJDStudios@users.noreply.github.com>
2024-09-14 18:55:02 +0100

    testimonials (#7)

--- src/__tests__/Testimonials.test.js
+++ src/__tests__/Testimonials.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Testimonials from '../pages/Testimonials';
 
-test('renders Testimonails page', () => {
+test('renders Testimonials page', () => {
   render(<Testimonials />);
   
   const pageTitle = screen.getByRole('heading', { name: /My Students Are Awesome/i });
   expect(pageTitle).toBeInTheDocument();
 
-  const pageContent = screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at mi nec purus tempor fringilla sed id lectus. Vivamus id mauris euismod, posuere felis sed, malesuada urna. Aliquam suscipit efficitur arcu, vel eleifend urna consequat a./i);
-  expect(pageContent).toBeInTheDocument();
+  const introText = screen.getByText(/No matter how many years I do this, students continue to inspire and humble me./i);
+  expect(introText).toBeInTheDocument();
+
+  const gratitudeSection = screen.getByRole('heading', { name: /Gratitude from My Students/i });
+  expect(gratitudeSection).toBeInTheDocument();
+
+  const classSection = screen.getByRole('heading', { name: /In Class and Beyond/i });
+  expect(classSection).toBeInTheDocument();
+
+  const funSection = screen.getByRole('heading', { name: /Fun in the Classroom/i });
+  expect(funSection).toBeInTheDocument();
 });
