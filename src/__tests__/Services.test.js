ab7f442f39fe79c4bbc278a8e22af20d0986a126
AD (AJDStudios) <105710407+AJDStudios@users.noreply.github.com>
2024-09-16 19:26:40 +0100

    calendly init (#12)

--- src/__tests__/Services.test.js
+++ src/__tests__/Services.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import Services from '../pages/Services';
 
-test('renders Services page', () => {
+test('renders Services page and checks content', () => {
   render(
     <MemoryRouter>
       <Services />
@@ -16,6 +17,11 @@ test('renders Services page', () => {
   const standardLesson = screen.getByText(/Standard Communication Lesson/i);
   expect(standardLesson).toBeInTheDocument();
 
-  const bookNowButtons = screen.getAllByText(/Book Now/i);
+  const bookNowButtons = screen.getAllByRole('button', { name: /Book Now/i });
   expect(bookNowButtons.length).toBeGreaterThan(0);
+
+  const firstBookNowButton = bookNowButtons[0];
+  userEvent.click(firstBookNowButton);
+
+  expect(firstBookNowButton).toBeEnabled();
 });
